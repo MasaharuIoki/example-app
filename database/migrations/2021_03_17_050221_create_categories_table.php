930793ece0cbf87c1396987f218bb8fbc29ae3ff
MasaharuIoki <m-ioki@bps21.jp>
2021-03-22 15:57:07 +0900

    up

--- database/migrations/2021_03_17_050221_create_categories_table.php
+++ database/migrations/2021_03_17_050221_create_categories_table.php
@@ -14,7 +14,8 @@ class CreateCategoriesTable extends Migration
     public function up()
     {
         Schema::create('categories', function (Blueprint $table) {
-            $table->id();
+            $table->inctements('id');
+            $teble->string('gender');
             $table->timestamps();
         });
     }
