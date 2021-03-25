3033ca777df268ce662227f1dbf3ba2e3affec7a
MasaharuIoki <m-ioki@bps21.jp>
2021-03-25 12:52:17 +0900

    updata

---
+++ app/Http/Controllers/QestionsListsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\QestionsLists;
+use Illuminate\Http\Request;
+
+class QestionsListsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\QestionsLists  $qestionsLists
+     * @return \Illuminate\Http\Response
+     */
+    public function show(QestionsLists $qestionsLists)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\QestionsLists  $qestionsLists
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(QestionsLists $qestionsLists)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\QestionsLists  $qestionsLists
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, QestionsLists $qestionsLists)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\QestionsLists  $qestionsLists
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(QestionsLists $qestionsLists)
+    {
+        //
+    }
+}
---
+++ database/factories/QestionsListsFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\QestionsLists;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class QestionsListsFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = QestionsLists::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
