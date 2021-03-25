7fb9edc35f98a7bb84323eafaea4572310a30597
MasaharuIoki <m-ioki@bps21.jp>
2021-03-25 13:50:08 +0900

    update

---
+++ app/Http/Controllers/QuestionsListsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\QuestionsLists;
+use Illuminate\Http\Request;
+
+class QuestionsListsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\QuestionsLists  $questionsLists
+     * @return \Illuminate\Http\Response
+     */
+    public function show(QuestionsLists $questionsLists)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\QuestionsLists  $questionsLists
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(QuestionsLists $questionsLists)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\QuestionsLists  $questionsLists
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, QuestionsLists $questionsLists)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\QuestionsLists  $questionsLists
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(QuestionsLists $questionsLists)
+    {
+        //
+    }
+}
--- database/seeders/QuestionsListsSeeder.php
+++ database/seeders/QuestionsListsSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
-class QestionsListsSeeder extends Seeder
+class QuestionsListsSeeder extends Seeder
 {
     /**
      * Run the database seeds.
