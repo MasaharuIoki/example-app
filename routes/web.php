8f20821f61c3d53730df7b9f25d1683f2662489c
MasaharuIoki <m-ioki@bps21.jp>
2021-03-25 10:36:02 +0900

    fix_#4

--- routes/web.php
+++ routes/web.php
@@ -9,5 +9,18 @@ Route::get('/', function () {
 Route::get('/questions', 'App\Http\Controllers\QuestionslistController@index');
 //Route::get('/questions', 'App\Http\Controllers\SampleValiRequest@index');
 // フォーム送信時のルーティング!
-Route::get('/questions', 'App\Http\Controllers\QuestionslistController@receiveData');
+Route::get('/questions/1', 'App\Http\Controllers\QuestionslistController@receiveData');
 //Route::get('/questions', 'App\Http\Controllers\SampleValiRequest@receiveData');
+Route:get('aaa', function () {
+
+    $item = new \App\Models\Questionslist;
+    
+    $item->choice1 = 1;
+    $item->save();
+     
+    // ※項目が足りて得ないのでエラーがでます。
+     
+    dump($item);
+     
+    return "";
+    });
